3a9f02c6de895a2cb031cb7cde479b656645d0d9
Connor Krenzer <crkrenz72@gmail.com>
2023-11-11 12:33:09 -0500

    gitignore and installation script

---
+++ debian_desktop.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/bash
+
+# Installs software I use in debian-based desktop environments (Linux Mint).
+
+
+function install_if_not_found(){
+    if dpkg -l | grep "^ii\ \+$1 "; then
+        echo "$1 already installed"
+    else
+        sudo apt install "$1" -y || echo "ISSUE INSTALLING $1"
+    fi
+}
+
+
+# PROGRAMMING LANGUAGES, COMPLIERS, ETC.
+install_if_not_found "buildapp" # used to create executables (works well with sbcl)
+install_if_not_found "gawk"
+install_if_not_found "gcc"
+install_if_not_found "julia"
+# <<RStudio dependencies from an installer>>
+install_if_not_found "lib32stdc++6"
+install_if_not_found "libclang-dev"
+install_if_not_found "libc6-i386"
+install_if_not_found "lib32gcc-s1"
+install_if_not_found "libllvm14"
+install_if_not_found "libobjc-11-dev"
+install_if_not_found "libclang1-14"
+install_if_not_found "libclang-common-14-dev"
+install_if_not_found "libobjc4"
+install_if_not_found "libclang-14-dev"
+# <<End RStudio dependencies from an installer>>
+install_if_not_found "lua5.4"
+install_if_not_found "mawk"
+install_if_not_found "openjdk-19-jdk" # replace the java version with default-jdk if you don't care about the version.
+                                      # sudo apt-get purge --autoremove openjdk* removes Java from your system
+install_if_not_found "python3"
+install_if_not_found "python3-pip"
+install_if_not_found "r-base-core"
+install_if_not_found "sbcl"
+
+
+# APPLICATIONS
+install_if_not_found "chromium-browser"
+install_if_not_found "firefox"
+install_if_not_found "libreoffice"
+install_if_not_found "obs-studio"
+install_if_not_found "pgadmin4"
+install_if_not_found "slack"
+install_if_not_found "tor"
+install_if_not_found "vlc-bin"
+install_if_not_found "zoom-player"
+
+
+# TOOLS AND UTILITIES
+install_if_not_found "adb"            # Used to connect my android phone to my computer for developer-mode debugging,
+                                      # which I primarily use for screencasting
+install_if_not_found "docker.io"
+install_if_not_found "ffmpeg"         # video and audio converter
+install_if_not_found "git"
+# <<R package installation dependencies>>
+install_if_not_found "libcurl4-openssl-dev"
+install_if_not_found "libudunits2-dev"
+install_if_not_found "libgdal-dev"
+# <<End R packages installation dependencies>>
+install_if_not_found "pass"           # needed for gpg
+install_if_not_found "maven"          # maven is required for the java LSP
+install_if_not_found "npm"            # mason.nvim requires npm to do the work installing LSPs
+install_if_not_found "parallel"
+install_if_not_found "poppler-utils"  # contains the pdftotext utility
+install_if_not_found "putty"
+install_if_not_found "scrcpy"         # I can screencast my phone onto the computer using this once the phone
+                                      # is connected to the adb server (preferably using a usb chord)
+install_if_not_found "tmux"
+install_if_not_found "xdotool"        # used in some vim functions to turn off caps lock
+
+
+# SPECIAL HANDLING
+# <<latest stable version of neovim>>
+if dpkg -l | grep "^ii\ \+neovim "; then
+    echo "neovim already installed"
+else
+    install_if_not_found "software-properties-common"
+    sudo add-apt-repository ppa:neovim-ppa/stable
+    sudo apt-get update
+    sudo apt-get install neovim
+fi
+# <<End latest stable version of neovim>>
+
+# <<display link driver installation>>
+# drivers from repo to use external monitors through docking stations (among other uses)
+synaptics_file="$HOME/Downloads/synaptics-repository-keyring.deb" # path to the repository on disk
+curl "https://www.synaptics.com/sites/default/files/Ubuntu/pool/stable/main/all/synaptics-repository-keyring.deb" -o "$synaptics_file"
+install_if_not_fouund "${synaptics_file}"
+install_if_not_found "displaylink-driver"
+# <<End display link driver installation>>
+
+# <<vimplug installation script>>
+test -e "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" || \
+    curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# <<End vimplug installation script>>
+
+# <<youtube-dl installation>>
+# This package is always out of date on apt. YouTube's actions make this tool unstable.
+# The pip installation is your best bet.
+pip3 list | grep "youtube-dl" || sudo pip3 install youtube-dl
+#  to update: sudo pip3 install --upgrade youtube-dl
+# <<End youtube-dl installation>>
+
+# <<quicklisp installation>>
+if test -e ~/quicklisp/setup.lisp; then
+    echo "quicklisp already installed"
+else
+    curl -O https://beta.quicklisp.org/quicklisp.lisp && \
+    curl -O https://beta.quicklisp.org/quicklisp.lisp.asc && \
+    gpg --verify quicklisp.lisp.asc quicklisp.lisp && \
+    sbcl --load quicklisp.lisp --eval "(quicklisp-quickstart:install)" --eval "(quit)" && \
+    rm quicklisp.lisp quicklisp.lisp.asc
+fi
+# <<End quicklisp installation>>
