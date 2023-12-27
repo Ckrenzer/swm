a896b4d98e019e3b33c1b5034a123f68fbccc575
Connor Krenzer <crkrenz72@gmail.com>
2023-12-26 20:51:32 -0500

    updating dependencies for mason.nvim

--- debian_desktop.sh
+++ debian_desktop.sh
@@ -23,7 +23,7 @@ function pip_install_if_not_found(){
 install_if_not_found "buildapp" # used to create executables (works well with sbcl)
 install_if_not_found "gawk"
 install_if_not_found "gcc"
-install_if_not_found "julia"
+install_if_not_found "golang-go"
 # <<RStudio dependencies from an installer>>
 install_if_not_found "lib32stdc++6"
 install_if_not_found "libclang-dev"
@@ -40,10 +40,13 @@ install_if_not_found "lua5.4"
 install_if_not_found "mawk"
 install_if_not_found "openjdk-19-jdk" # replace the java version with default-jdk if you don't care about the version.
                                       # sudo apt-get purge --autoremove openjdk* removes Java from your system
+install_if_not_found "php-cli"
 install_if_not_found "python3"
 install_if_not_found "python3-pip"
 install_if_not_found "python3-venv"
 install_if_not_found "r-base-core"
+install_if_not_found "ruby"
+install_if_not_found "rustc"
 install_if_not_found "sbcl"
 
 
@@ -63,14 +66,18 @@ install_if_not_found "zoom-player"
 # TOOLS AND UTILITIES
 install_if_not_found "adb"            # Used to connect my android phone to my computer for developer-mode debugging,
                                       # which I primarily use for screencasting
+install_if_not_found "cargo"          # rust package manager
+install_if_not_found "composer"       # php package manager
 install_if_not_found "docker.io"
 install_if_not_found "ffmpeg"         # video and audio converter
+install_if_not_found "gem"            # ruby package manager
 install_if_not_found "git"
 # <<R package installation dependencies>>
 install_if_not_found "libcurl4-openssl-dev"
 install_if_not_found "libudunits2-dev"
 install_if_not_found "libgdal-dev"
 # <<End R packages installation dependencies>>
+install_if_not_found "luarocks"       # lua package manager
 install_if_not_found "pass"           # needed for gpg
 install_if_not_found "maven"          # maven is required for the java LSP
 install_if_not_found "npm"            # mason.nvim requires npm to do the work installing LSPs
@@ -84,6 +91,8 @@ install_if_not_found "xdotool"        # used in some vim functions to turn off c
 
 
 # PIP3 INSTALLS
+pip3 list | grep "^jill\ " || \
+    sudo pip install jill --user -U
 pip_install_if_not_found "numpy"
 pip_install_if_not_found "pandas"
 # youtube-dl is always out of date on apt.
@@ -110,6 +119,12 @@ sudo Rscript -e "pkgs <- commandArgs(trailingOnly = TRUE)
 
 
 # SPECIAL HANDLING
+# <<ensure npm is up-to-date>>
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+#<<End ensure npm is up-to-date>>
+
 # <<latest stable version of neovim>>
 if dpkg -l | grep "^ii\ \+neovim "; then
     echo "neovim already installed"
@@ -134,6 +149,10 @@ test -e "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" || \
     curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # <<End vimplug installation script>>
+
+# <<julia installation>>
+jill install # requires ${HOME}/.local/bin to be on PATH for this to work
+# <<End juliat installation>>
 
 # <<quicklisp installation>>
 if test -e ~/quicklisp/setup.lisp; then
